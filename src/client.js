a8a71c0a2333dc0e58d57b7101a5bea12d15aff5
Espen Gjendem <espen.gjendem@gmail.com>
2021-11-12 18:32:43 +0100

    done

--- src/client.js
+++ src/client.js
@@ -2,7 +2,6 @@ const { JSONRPCClient } = require("json-rpc-2.0");
 const axios = require('axios');
 
 const client = new JSONRPCClient((request) => { 
-  console.log(request);
   return axios({
   method: 'post',
   url: process.env.REACT_APP_BOOKING_API_URL,
