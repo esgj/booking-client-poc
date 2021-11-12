f9b3a6eaa1697bffa06e4e9ab302493359516748
Espen Gjendem <espen.gjendem@gmail.com>
2021-11-12 18:45:11 +0100

    done

--- src/client.js
+++ src/client.js
@@ -2,16 +2,20 @@ const { JSONRPCClient } = require("json-rpc-2.0");
 const axios = require('axios');
 
 const client = new JSONRPCClient((request, { token }) => { 
+  let headers = {
+    'Content-Type': 'application/json'
+  }
+
+  if (token) headers['Authorization'] = `Bearer ${token}`;
+
   return axios({
   method: 'post',
   url: process.env.REACT_APP_BOOKING_API_URL,
-  headers: {
-    'Content-Type': 'application/json'
-  },
+  headers,
   data: JSON.stringify(request)
 }).then(response => client.receive(response.data))});
 
 module.exports = {
-  getAllBookings: () => client.request('getBookings'),
-  addBooking: (value) => client.request('addBooking', value)
+  getAllBookings: () => client.request('getBookings', null, { token: null }),
+  addBooking: (value) => client.request('addBooking', value, { token: value.token })
 }
