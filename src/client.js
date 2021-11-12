9696134c160a407f4eaa382c44a047ef38fbb332
Espen Gjendem <espen.gjendem@gmail.com>
2021-11-12 10:44:02 +0100

    test

--- src/client.js
+++ src/client.js
@@ -1,1 +1,13 @@
-module.exports.test = async () => 'Hello';
+const { JSONRPCClient } = require("json-rpc-2.0");
+const axios = require('axios');
+
+const client = new JSONRPCClient((request) => axios({
+  method: 'post',
+  url: 'http://localhost:3000/json-rpc',
+  headers: {
+    'content-type': 'application/json'
+  },
+  data: JSON.stringify(request)
+}).then(response => client.receive(response.data)));
+
+module.exports.test = () => client.request('echo', { text: '123' }).then(d => console.log(d)).catch(err => console.log(err));
