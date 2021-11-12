5c19130c66bd5d9096d652f66403596614786370
Espen Gjendem <espen.gjendem@gmail.com>
2021-11-12 11:22:10 +0100

    first version

--- src/client.js
+++ src/client.js
@@ -1,13 +1,18 @@
 const { JSONRPCClient } = require("json-rpc-2.0");
 const axios = require('axios');
 
-const client = new JSONRPCClient((request) => axios({
+const client = new JSONRPCClient((request) => { 
+  console.log(request);
+  return axios({
   method: 'post',
-  url: process.env.BOOKING_JSONRPC_API_URL,
+  url: process.env.BOOKING_API_URL,
   headers: {
     'content-type': 'application/json'
   },
   data: JSON.stringify(request)
-}).then(response => client.receive(response.data)));
+}).then(response => client.receive(response.data))});
 
-module.exports.test = () => client.request('echo', { text: '123' }).then(d => console.log(d)).catch(err => console.log(err));
+module.exports = {
+  getAllBookings: () => client.request('getBookings'),
+  bookingClient: client
+}
